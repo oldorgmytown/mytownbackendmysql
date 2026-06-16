58e329ddcbc452a3c5038979b39cdf9741a7766f
siri-itismytown <siri@itismytown.com>
2026-06-16 16:07:23 +0530

    commit_QAlatest

--- mytown/DataAccess/Repositories/TransporterDashboardRepository.cs
+++ mytown/DataAccess/Repositories/TransporterDashboardRepository.cs
@@ -547,7 +547,7 @@ public async Task<TravelPlanDto> SaveTravelPlanAsync(TravelPlanDto dto)
 
             await _context.SaveChangesAsync();
 
-            // send tracking id email to guests
+            // send tracking id email to guests only
             if (dto.NewStatus == "PickedUp" &&
               delivery.OrderId.HasValue)
             {
