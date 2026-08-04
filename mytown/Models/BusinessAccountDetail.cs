0cf880b31f17ce5493d30ed647d83c2b119a1af3
siri-itismytown <siri@itismytown.com>
2026-08-04 09:36:43 +0530

    commit_razorpayx

--- mytown/Models/BusinessAccountDetail.cs
+++ mytown/Models/BusinessAccountDetail.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using MyTown.Enums;
+
 
 namespace MyTown.Models
 {
@@ -33,7 +35,67 @@ namespace MyTown.Models
         [StringLength(20)]
         [Column("ifsc_code")]
         public string IFSCCode { get; set; }
+
+        #region Bank Verification
+
+        [StringLength(20)]
+        [Column("bank_verification_status")]
+        public BankVerificationStatus BankVerificationStatus { get; set; }
+    = BankVerificationStatus.Pending;
+
+        [Column("bank_verified_date")]
+        public DateTime? BankVerifiedDate { get; set; }
+
+        [StringLength(100)]
+        [Column("bank_verification_reference")]
+        public string? BankVerificationReference { get; set; }
+
+        [StringLength(500)]
+        [Column("bank_verification_message")]
+        public string? BankVerificationMessage { get; set; }
+
+        #endregion
+
+        #region Razorpay Contact
+
+        [StringLength(100)]
+        [Column("razorpay_contact_id")]
+        public string? RazorpayContactId { get; set; }
+
+        [StringLength(20)]
+        [Column("razorpay_contact_status")]
+        public RazorpayContactStatus RazorpayContactStatus { get; set; }
+    = RazorpayContactStatus.NotCreated;
+
        
+
+        [Column("contact_created_date")]
+        public DateTime? ContactCreatedDate { get; set; }
+
+        #endregion
+
+        #region Razorpay Fund Account
+
+        [StringLength(100)]
+        [Column("razorpay_fund_account_id")]
+        public string? RazorpayFundAccountId { get; set; }
+
+        [StringLength(20)]
+        [Column("razorpay_fund_account_status")]
+        public RazorpayFundAccountStatus RazorpayFundAccountStatus { get; set; }
+            = RazorpayFundAccountStatus.NotCreated;
+
+        [Column("fund_account_created_date")]
+        public DateTime? FundAccountCreatedDate { get; set; }
+
+        #endregion
+
+        [Column("is_active")]
+        public bool IsActive { get; set; } = true;
+
+        [StringLength(500)]
+        [Column("remarks")]
+        public string? Remarks { get; set; }
 
         [Column("created_date")]
         public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
