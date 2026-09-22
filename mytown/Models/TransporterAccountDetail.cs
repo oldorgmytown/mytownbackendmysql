560c3e811598c6617fe180f5248fc55a151319fa
siri-itismytown <siri@itismytown.com>
2026-09-22 19:26:17 +0530

    commit_missingsinprod

--- mytown/Models/TransporterAccountDetail.cs
+++ mytown/Models/TransporterAccountDetail.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-//push
+//push to prod
 namespace mytown.Models
 {
     [Table("transporter_account_details")]
